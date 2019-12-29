7749075a7bd689604f66750aef5696cc5f60dd62
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-12-29 03:32:37 +0300

    Компонент Алерт использованы Хуки

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AlertContext} from '../context/alert/alertContext'
 
 export const Search = () => {
+  const {show} = useContext(AlertContext)
+
+  const onSubmit = event => {
+    if (event.key === 'Enter') {
+      show('This is ALERT!')
+    }
+  }
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        onKeyPress={onSubmit}
       />
     </div>
   )
