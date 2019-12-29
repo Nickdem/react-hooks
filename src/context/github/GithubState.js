ea75b873a9debb4a641bc494a3c220e81908b8da
Nickdem <45073853+Nickdem@users.noreply.github.com>
2019-12-30 02:06:23 +0300

    Настройка запросов

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -6,6 +6,9 @@ import {SEARCH_USERS, GET_REPOS, GET_USER, SET_LOADING, CLEAR_USERS} from '../ty
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+const withCreds = url => {
+  return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
 
 export const GithubState = ({children}) => {
   const initialState = {
@@ -16,12 +19,12 @@ export const GithubState = ({children}) => {
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
-  
+
   const search = async value => {
     setLoading()
 
     const responce = await axios.get(
-      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      withCreds(`https://api.github.com/search/users?q=${value}&`)
     )
 
     dispatch({
@@ -29,27 +32,35 @@ export const GithubState = ({children}) => {
       payload: responce.data.items
     })
   }
-  
+
   const getUser = async name => {
     setLoading()
 
+    const responce = await axios.get(
+      withCreds(`https://api.github.com/users/${name}?`)
+    )
+
     dispatch({
       type: GET_USER,
-      payload: {}
+      payload: responce.data
     })
   }
-  
+
   const getRepos = async name => {
     setLoading()
 
+    const responce = await axios.get(
+      withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+    )
+
     dispatch({
       type: GET_REPOS,
-      payload: []
+      payload: responce.data
     })
   }
-  
+
   const clearUser = () => dispatch({type: CLEAR_USERS})
-  
+
   const setLoading = () => dispatch({type: SET_LOADING})
 
   const {user, users, repos, loading} = state
